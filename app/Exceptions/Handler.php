1005866f2c419135f86d65c7347ab82da355df76
Dina Essam <dina.essam.fcis@gmail.com>
2022-03-04 04:45:43 +0200

    handle types of errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,10 +3,20 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
 use Throwable;
+use App\Traits\ApiResponser;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -35,7 +45,44 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+            return false;
         });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     */
+    public function render($request, Exception|Throwable $e): JsonResponse
+    {
+        $response = $this->handleException($request, $e);
+        return $response;
+    }
+
+
+    /**
+     * @throws Throwable
+     */
+    public function handleException($request, Exception|Throwable $exception): JsonResponse
+    {
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse(405,'The specified method for the request is invalid');
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            return $this->errorResponse(404,'The specified URL cannot be found');
+        }
+
+        if ($exception instanceof HttpException) {
+            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+
+        if (config('app.debug')) {
+            return parent::render($request, $exception);
+        }
+
+        return $this->errorResponse(500,'Unexpected Exception. Try later');
+
     }
 }
