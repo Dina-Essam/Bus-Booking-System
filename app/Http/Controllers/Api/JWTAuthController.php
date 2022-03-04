4e34f11d15b401f06f1adac2f8b4d73434e85cf1
Dina Essam <dina.essam.fcis@gmail.com>
2022-03-04 05:22:09 +0200

    create authentication controller

---
+++ app/Http/Controllers/Api/JWTAuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+use function PHPUnit\Framework\returnArgument;
+
+class JWTAuthController extends Controller
+{
+
+    public function register(Request $request): JsonResponse {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return $this->successResponse($response);
+    }
+
+    public function login(Request $request): JsonResponse
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+
+        if (!Auth::attempt($fields)) {
+            return $this->errorResponse(ResponseAlias::HTTP_UNAUTHORIZED,'Invalid Email or Password');
+        }
+
+        $user = User::where('email', $fields['email'])->first();
+
+        if ( !$user || !Hash::check($fields['password'], $user->password)) {
+            return $this->errorResponse(ResponseAlias::HTTP_INTERNAL_SERVER_ERROR,'Error in Login');
+        }
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return $this->successResponse($response);
+    }
+
+    public function logout(Request $request): JsonResponse {
+        auth()->user()->tokens()->delete();
+        return $this->successResponse([],"Logged out");
+    }
+
+}
