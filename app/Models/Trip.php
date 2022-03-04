569ca9c9431d64b8ff4beb8581efc4f68c4d76f8
Dina Essam <dina.essam.fcis@gmail.com>
2022-03-04 22:53:13 +0200

    main methods in trip

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use phpDocumentor\Reflection\Types\Boolean;
+use phpDocumentor\Reflection\Types\This;
 
 class Trip extends Model
 {
@@ -25,6 +27,34 @@ class Trip extends Model
     public function bookings()
     {
         return $this->hasMany(Booking::class,'trip_id');
+    }
+
+    public function hasAvailableSeats(Station $source , Station $destination):Boolean
+    {
+        return count($this->getSeatsReserved($source,$destination))<12;
+    }
+
+    public function getSeatsReserved(Station $source , Station $destination)
+    {
+        return $this->bookings->where(
+            function ($query) use ($source,$destination){
+                $query->source_station()->where('order','>=',$source->order)
+                    ->where('order','<=',$destination->order);
+            }
+        )->orWhere(
+            function ($query) use ($source,$destination){
+                $query->destination_station()->where('order','>=',$source->order)
+                    ->where('order','<=',$destination->order);
+            }
+        )->seatsMapping->seat()->get();
+    }
+
+    public function getAvailableSeats(Station $source , Station $destination)
+    {
+        $reserved = $this->getSeatsReserved($source , $destination);
+        $allSeats = $this->bus->seats()->get();
+        $available_seats = $allSeats->diff($reserved);
+        return $available_seats->all();
     }
 
 
