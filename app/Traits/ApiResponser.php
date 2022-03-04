8757123ebc82a866806c033724596c18b3aec698
Dina Essam <dina.essam.fcis@gmail.com>
2022-03-04 04:40:48 +0200

    create a general JSON structure for this entire project Response

---
+++ app/Traits/ApiResponser.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+
+trait ApiResponser{
+
+    protected function successResponse($data, $message = null, $code = 200): JsonResponse
+    {
+        return response()->json([
+            'status'=> 'Success',
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    protected function errorResponse($code, $message = null): JsonResponse
+    {
+        return response()->json([
+            'status'=>'Error',
+            'message' => $message,
+            'data' => null
+        ], $code);
+    }
+
+}
